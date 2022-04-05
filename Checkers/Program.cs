540676384a2a33bc783012647c18f4aa5baf36ce
BernardoAguiar <bernardo.aguiar@bairesdev.com>
2022-04-05 14:54:23 -0300

    Clean program

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -6,9 +6,7 @@ namespace Checkers
     {
         static void Main(string[] args)
         {
-            int[,] board = new int[8, 8] { { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 2, 0, 2, 0, 2, 0, 2, 0 }, { 0, 2, 0, 2, 0, 2, 0, 2 }, { 2, 0, 2, 0, 2, 0, 2, 0 } };
-            Graphics.SplashScreen();
-            Graphics.DrawBoard(board);
+            
         }
     }
 }
